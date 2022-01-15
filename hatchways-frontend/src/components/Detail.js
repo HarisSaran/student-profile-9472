2c20ae5b856a3bbfb322cc6fbca5d1fd8a554e10
Haris Saran <harissaran33@gmail.com>
2022-01-14 19:28:36 -0600

    adding style

--- hatchways-frontend/src/components/Detail.js
+++ hatchways-frontend/src/components/Detail.js
@@ -3,16 +3,17 @@ import { BiPlusMedical } from "react-icons/bi";
 const Detail = ({ detail }) => {
   return (
     <div className="detail">
-      <h2>
-        {detail.firstName + " " + detail.lastName}{" "}
+      <h1>
+        {detail.firstName.toUpperCase() + " " + detail.lastName.toUpperCase()}{" "}
         <BiPlusMedical style={{ color: "grey" }} />
-      </h2>
-      <p>Email: {detail.email}</p>
-      <p>Company: {detail.company}</p>
-      <p>Skill: {detail.skill}</p>
-      <p>Average: {detail.skill}</p>
-      <img src={detail.pic} alt="oops, nothing found!" />
-      {/* <p>{detail.pic}</p> */}
+      </h1>
+      <div className="initial">
+        <img src={detail.pic} alt="oops, nothing found!" />
+        <p>Email: {detail.email} </p>
+        <p>Company: {detail.company}</p>
+        <p>Skill: {detail.skill}</p>
+        <p>Average: {detail.skill}</p>
+      </div>
     </div>
   );
 };
